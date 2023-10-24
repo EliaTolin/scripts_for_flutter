cbffc73b6a539ebb9e37bfd9883e4ac3a222d281
Elia Tolin <eliatolin@gmail.com>
2023-10-24 15:00:40 +0200

    Create build_test.sh

---
+++ build_test.sh
@@ -0,0 +1,40 @@
+echo "#### FLUTTER CLEAN ####" &&
+flutter clean &&
+echo "#### FLUTTER PUB GET ####" &&
+flutter pub get &&
+echo "#### FLUTTER GEN-L10N ####" &&
+flutter gen-l10n &&
+echo "#### TEST  ####" &&
+
+if ! flutter test; then
+    echo "#### TEST FAIL ####"
+    exit 1  
+fi
+
+echo "### INCREMENT VERSION ###" &&
+# Estrae la versione corrente dal file pubspec.yaml
+VERSION=$(grep 'version: ' pubspec.yaml | sed 's/version: //' | tr -d '\n' | tr -d '\r' | cut -d" " -f1)
+
+# Estrae la build e la minor versione
+MAJOR=$(echo $VERSION | cut -d. -f1)
+MINOR=$(echo $VERSION | cut -d. -f2)
+PATCH=$(echo $VERSION | cut -d. -f3 | cut -d+ -f1)
+BUILD=$(echo $VERSION | cut -d+ -f2)
+
+# Incrementa la build e la minor versione
+BUILD=$((BUILD + 1))
+
+# Costruisce la nuova versione
+NEW_VERSION="$MAJOR.$MINOR.$PATCH+$BUILD"
+
+# Sostituisce la vecchia versione con la nuova nel file pubspec.yaml
+sed -i '' "s/version: $VERSION/version: $NEW_VERSION/" pubspec.yaml
+
+# Stampa la nuova versione
+echo "FROM $VERSION TO $NEW_VERSION" &&
+
+echo "#### BUILD IOS ####" &&
+flutter build ipa --dart-define=ENV=test && 
+echo "#### BUILD ANDROID ####" &&
+flutter build appbundle --dart-define=ENV=test &&
+echo "#### FINISH ####"
